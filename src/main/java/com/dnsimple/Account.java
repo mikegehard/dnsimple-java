0f7e883c08f5d2280c3120b9f6ac5eb7f45f7ee5
Mike Gehard <mike.gehard@gmail.com>
2016-10-05 19:57:38 -0600

    try kotlin
    The Goolge HTTP library breaks so it might require a new library

--- src/main/java/com/dnsimple/Account.java
+++ src/main/java/com/dnsimple/Account.java
@@ -1,21 +1,21 @@
-package com.dnsimple;
-
-import com.google.api.client.util.Key;
-
-public class Account {
-  @Key("id")
-  private Integer id;
-
-  @Key("email")
-  private String email;
-
-  public Account() {}
-
-  public Integer getId() {
-    return id;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-}
+//package com.dnsimple;
+//
+//import com.google.api.client.util.Key;
+//
+//public class Account {
+//  @Key("id")
+//  private Integer id;
+//
+//  @Key("email")
+//  private String email;
+//
+//  public Account() {}
+//
+//  public Integer getId() {
+//    return id;
+//  }
+//
+//  public String getEmail() {
+//    return email;
+//  }
+//}
---
+++ src/main/java/com/dnsimple/Account.kt
@@ -0,0 +1,6 @@
+package com.dnsimple
+
+import com.google.api.client.util.Key
+
+
+class Account (@Key val id: Int, @Key val email: String)
